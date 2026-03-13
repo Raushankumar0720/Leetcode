08cff8af793a0ffea1d12b7657920671af9bd595
Raushan kumar <raushan.singh.cg@gmail.com>
2026-03-14 00:12:08 +0530

    File updated at 3/14/2026, 12:12:08 AM

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,7 +1,30 @@
-// Last updated: 3/14/2026, 12:02:05 AM
-1class Solution {
-2public:
-3    void sortColors(vector<int>& nums) {
-4        sort(nums.begin(),nums.end());
-5    }
-6};
+// Last updated: 3/14/2026, 12:12:08 AM
+1// Brute force
+2
+3class Solution {
+4public:
+5    void sortColors(vector<int>& nums) {
+6        int low = 0, mid = 0, high = nums.size() - 1;
+7        
+8        while (mid <= high) {
+9            if (nums[mid] == 0) {
+10                swap(nums[low], nums[mid]);
+11                low++; mid++;
+12            } else if (nums[mid] == 1) {
+13                mid++;
+14            } else { // nums[mid] == 2
+15                swap(nums[mid], nums[high]);
+16                high--;
+17            }
+18        }
+19    }
+20};
+21
+22// Using stl
+23
+24// class Solution {
+25// public:
+26//     void sortColors(vector<int>& nums) {
+27//         sort(nums.begin(),nums.end());
+28//     }
+29// };
