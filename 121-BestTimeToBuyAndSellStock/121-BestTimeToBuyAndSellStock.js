4ea58b12a7276ea977db621da465d63d5df47313
Raushan kumar <raushan.singh.cg@gmail.com>
2026-03-18 23:03:49 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.js
@@ -0,0 +1,20 @@
+// Last updated: 3/18/2026, 11:03:48 PM
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i]; // update minimum
+    } else {
+      maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+  }
+
+  return maxProfit;
+
+};
