2fa0c917abd7a8326b8faa3869afb99726cc68fc
Raushan kumar <raushan.singh.cg@gmail.com>
2026-04-07 09:44:25 +0530

    File updated at 4/7/2026, 9:44:25 AM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,18 +1,18 @@
-// Last updated: 4/7/2026, 9:42:05 AM
+// Last updated: 4/7/2026, 9:44:25 AM
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
-4        int candidate = 0;
+4        int element = 0;
 5        int count = 0;
 6
 7        for (int num : nums) {
 8            if (count == 0) {
-9                candidate = num;
+9                element = num;
 10            }
-11            count += (num == candidate) ? 1 : -1;
+11            count += (num == element) ? 1 : -1;
 12        }
 13
-14        return candidate;
+14        return element;
 15    }
 16};
 17
