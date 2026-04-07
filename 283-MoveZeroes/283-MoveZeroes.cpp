55009c4a06c108d6afb2c973d3b6a0f53cea0adb
Raushan kumar <raushan.singh.cg@gmail.com>
2026-04-07 10:10:53 +0530

    File updated at 4/7/2026, 10:10:54 AM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,20 +1,18 @@
-// Last updated: 3/27/2026, 11:55:23 PM
+// Last updated: 4/7/2026, 10:10:54 AM
 1class Solution {
 2public:
 3    void moveZeroes(vector<int>& nums) {
 4        int index = 0;
 5        
-6        // Step 1: Copy non-zero elements forward
-7        for (int i = 0; i < nums.size(); i++) {
-8            if (nums[i] != 0) {
-9                nums[index++] = nums[i];
-10            }
-11        }
-12        
-13        // Step 2: Fill remaining positions with zeros
-14        while (index < nums.size()) {
-15            nums[index++] = 0;
-16        }
-17    }
-18};
-19
+6        for (int i = 0; i < nums.size(); i++) {
+7            if (nums[i] != 0) {
+8                nums[index++] = nums[i];
+9            }
+10        }
+11        
+12        while (index < nums.size()) {
+13            nums[index++] = 0;
+14        }
+15    }
+16};
+17
