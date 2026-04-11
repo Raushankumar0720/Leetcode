6d5574339ea13bc3af953017478ca199c7e2aa9d
Raushan kumar <raushan.singh.cg@gmail.com>
2026-04-11 15:37:41 +0530

    File updated at 4/11/2026, 3:37:41 PM

--- 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.cpp
+++ 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.cpp
@@ -1,18 +1,14 @@
-// Last updated: 4/11/2026, 3:35:31 PM
+// Last updated: 4/11/2026, 3:37:41 PM
 1class Solution {
 2public:
 3    bool uniqueOccurrences(vector<int>& arr) {
-4        unordered_map<int,int> count;
-5        for (int num : arr) count[num]++;
-6
-7        vector<int> freq;
-8        for (auto &p : count) freq.push_back(p.second);
-9
-10        sort(freq.begin(), freq.end());
-11        for (int i = 1; i < freq.size(); i++) {
-12            if (freq[i] == freq[i-1]) return false;
-13        }
-14        return true;
-15    }
-16};
-17
+4        unordered_map<int, int> count;
+5        unordered_set<int> occurrences;
+6        for (int num : arr) count[num]++;
+7        for (auto& [key, freq] : count) {
+8            if (!occurrences.insert(freq).second) return false;
+9        }
+10        return true;
+11    }
+12};
+13
