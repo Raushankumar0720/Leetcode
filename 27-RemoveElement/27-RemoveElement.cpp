b2486e7fdff30b5c8da9be353dc82efa8959c88b
Raushan kumar <raushan.singh.cg@gmail.com>
2026-04-12 23:12:09 +0530

    File updated at 4/12/2026, 11:12:09 PM

--- 27-RemoveElement/27-RemoveElement.cpp
+++ 27-RemoveElement/27-RemoveElement.cpp
@@ -1,15 +1,15 @@
-// Last updated: 4/12/2026, 11:11:03 PM
+// Last updated: 4/12/2026, 11:12:09 PM
 1class Solution {
 2public:
 3    int removeElement(vector<int>& nums, int val) {
-4    int k = 0; // position for next non-val element
+4    int k = 0; 
 5    for (int i = 0; i < nums.size(); i++) {
 6        if (nums[i] != val) {
 7            nums[k] = nums[i];
 8            k++;
 9        }
 10    }
-11    return k; // new length
+11    return k; 
 12}
 13
 14};
