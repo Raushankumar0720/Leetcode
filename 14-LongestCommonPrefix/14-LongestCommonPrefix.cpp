739bfd96cf8b9d961e66725e61fdb37a5572eb28
Raushan kumar <raushan.singh.cg@gmail.com>
2026-04-14 15:37:27 +0530

    File updated at 4/14/2026, 3:37:27 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -1,19 +1,19 @@
-// Last updated: 4/14/2026, 3:26:40 PM
+// Last updated: 4/14/2026, 3:37:27 PM
 1class Solution {
 2public:
 3    string longestCommonPrefix(vector<string>& strs) {
-4        if (strs.empty()) return "";
-5
-6        string prefix = strs[0]; // assume first string is prefix
-7
-8        for (int i = 1; i < strs.size(); i++) {
-9            // jab tak current string prefix se start nahi hoti
-10            while (strs[i].find(prefix) != 0) {
-11                prefix.pop_back(); // ek character hatao
-12                if (prefix.empty()) return ""; // agar khali ho gaya toh no prefix
-13            }
-14        }
-15        return prefix;
-16    }
-17};
-18
+4        if (strs.empty())
+5            return "";
+6
+7        string Prefix = strs[0];
+8
+9        for (int i = 1; i < strs.size(); i++) {
+10            while (strs[i].find(Prefix) != 0) {
+11                Prefix.pop_back();
+12                if (Prefix.empty())
+13                    return "";
+14            }
+15        }
+16        return Prefix;
+17    }
+18};
