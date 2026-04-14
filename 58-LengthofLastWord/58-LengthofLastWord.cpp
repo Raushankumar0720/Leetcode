c2a768619e02d24745b8353e3335873fa5623be7
Raushan kumar <raushan.singh.cg@gmail.com>
2026-04-14 15:57:10 +0530

    File updated at 4/14/2026, 3:57:09 PM

--- 58-LengthofLastWord/58-LengthofLastWord.cpp
+++ 58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -1,23 +1,20 @@
-// Last updated: 3/22/2026, 11:58:22 PM
+// Last updated: 4/14/2026, 3:57:09 PM
 1class Solution {
 2public:
 3   int lengthOfLastWord(string s) {
 4    int n = s.size();
-5    int i = n - 1;
+5    int i = n-1;
 6
-7    // Step 1: Skip trailing spaces
-8    while (i >= 0 && s[i] == ' ') {
-9        i--;
-10    }
-11
-12    // Step 2: Count length of last word
-13    int length = 0;
-14    while (i >= 0 && s[i] != ' ') {
-15        length++;
-16        i--;
-17    }
+7    while (i>=0 && s[i] == ' '){
+8        i--;
+9    }
+10
+11    int length =0 ;
+12    while(i>=0 && s[i] != ' '){
+13        length++;
+14        i--;
+15    }
+16    return length;
+17   }
 18
-19    return length;
-20   }
-21
-22};
+19};
