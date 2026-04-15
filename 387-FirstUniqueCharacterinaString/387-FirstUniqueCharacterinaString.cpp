f48e7ef4303b8a2fc828d081ae8316f09ca54c7a
Raushan kumar <raushan.singh.cg@gmail.com>
2026-04-15 11:51:13 +0530

    File updated at 4/15/2026, 11:51:13 AM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
@@ -1,15 +1,14 @@
-// Last updated: 4/15/2026, 11:19:36 AM
-1class Solution {
-2public:
-3    int firstUniqChar(string s) {
-4        unordered_map<char,int> occ ;
-5        for(char ch : s){
-6            occ[ch]++;
-7        }
-8
-9        for(int i=0; i<s.size(); i++){
-10            if(occ[s[i]] == 1) return i;
-11        }
-12        return -1;
-13    }
-14};
+// Last updated: 4/15/2026, 11:51:13 AM
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>freq(26,0) ;
+        for(auto c:s) {
+            freq[c-'a']++ ;
+        }
+        for(int i=0;i<s.size();i++) {
+            if(freq[s[i]-'a']==1) return i ;
+        }
+        return -1 ;
+    }
+};
