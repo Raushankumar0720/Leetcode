efb63c5432b2ca678a30206fef7614a452bfe556
Raushan kumar <raushan.singh.cg@gmail.com>
2026-05-03 22:01:51 +0530

    Create 2798-NumberofEmployeesWhoMettheTarget

---
+++ 2798-NumberofEmployeesWhoMettheTarget/2798-NumberofEmployeesWhoMettheTarget.cpp
@@ -0,0 +1,11 @@
+// Last updated: 5/3/2026, 10:01:50 PM
+1class Solution {
+2public:
+3    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+4        int count = 0;
+5        for(int h : hours) {
+6            if(h >= target) count++;
+7        }
+8        return count;
+9    }
+10};
