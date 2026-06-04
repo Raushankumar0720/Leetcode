cea7312b790d71619c12025d417de6d00533a096
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-04 11:53:40 +0530

    File updated at 6/4/2026, 11:53:40 AM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
@@ -1,13 +1,13 @@
-// Last updated: 6/4/2026, 11:51:43 AM
+// Last updated: 6/4/2026, 11:53:40 AM
 1class Solution {
 2public:
 3    int evalRPN(vector<string>& tokens) {
-4        stack<int> st;
+4        stack<long long> st;
 5
 6        for(string nuc : tokens){
 7            if(nuc == "+" || nuc == "-" || nuc == "*" || nuc == "/"){
-8                int b = st.top() ; st.pop();
-9                int a = st.top() ; st.pop();
+8                long long b = st.top() ; st.pop();
+9                long long a = st.top() ; st.pop();
 10
 11                if(nuc == "+") st.push(a+b);
 12                else if(nuc == "-") st.push(a-b);
