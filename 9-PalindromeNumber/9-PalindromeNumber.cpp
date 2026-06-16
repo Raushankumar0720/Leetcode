9a771832f4b507b2ec76b857285a41e3253a2aad
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-16 23:03:56 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/16/2026, 11:03:56 PM
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return (x == reversed || x == reversed / 10);
+    }
+};
