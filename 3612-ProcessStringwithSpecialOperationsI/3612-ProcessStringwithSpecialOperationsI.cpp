6032a1d440c064cbea24fb00b57f0263b7dabb19
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-17 11:05:39 +0530

    File updated at 6/17/2026, 11:05:39 AM

--- 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.cpp
+++ 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.cpp
@@ -1,21 +1,22 @@
-// Last updated: 6/17/2026, 11:02:37 AM
+// Last updated: 6/17/2026, 11:05:39 AM
 1class Solution {
 2public:
 3    string processStr(string s) {
-4        string result = "" ;
+4        string res = "";
 5
-6
-7        for (char c : s) {
-8            if (isalpha(c)) {
-9                result.push_back(c);
-10            } else if (c == '*') {
-11                if (!result.empty()) result.pop_back();
-12            } else if (c == '#') {
-13                result += result;
-14            } else if (c == '%') {
-15                reverse(result.begin(), result.end());
-16            }
-17        }
-18        return result;
-19    }
-20};
+6        for (char ch : s) {
+7            if (ch == '#') {
+8                res += res;
+9            } else if (ch == '%') {
+10                reverse(res.begin(), res.end());
+11            } else if (ch == '*') {
+12                if (!res.empty()) {
+13                    res.pop_back();
+14                }
+15            } else {
+16                res += ch;
+17            }
+18        }
+19        return res;
+20    }
+21};
