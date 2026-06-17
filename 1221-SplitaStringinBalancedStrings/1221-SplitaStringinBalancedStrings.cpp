d6cf2f14d8911240e0ba3930a4b932eaf74763e9
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-17 11:25:30 +0530

    File updated at 6/17/2026, 11:25:30 AM

--- 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cpp
+++ 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cpp
@@ -1,18 +1,14 @@
-// Last updated: 6/17/2026, 11:18:36 AM
-1class Solution {
-2public:
-3    int balancedStringSplit(string s) {
-4        int countL = 0 , countR = 0 , res = 0;
-5
-6        for(char ch : s){
-7            if(ch == 'L') countL++;
-8            else countR++;
-9            
-10            if(countL == countR){
-11                res++;
-12                countL = countR = 0;
-13            }
-14        }
-15        return res;
-16    }
-17};
+// Last updated: 6/17/2026, 11:25:30 AM
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int l=0,r=0,cnt=0;
+
+        for(auto& c:s){
+            if(c=='L')l++;
+            else r++;
+            if(l==r)cnt++;
+        }
+        return cnt;
+    }
+};
