dae2587cbeae53747f7b8007af7de58eaf1e6849
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-26 22:29:31 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.cpp
@@ -0,0 +1,10 @@
+// Last updated: 6/26/2026, 10:29:31 PM
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i=1; i<=n; i++)
+            ans.push_back(i%15==0 ? "FizzBuzz" : i%3==0 ? "Fizz" : i%5==0 ? "Buzz" : to_string(i));
+        return ans;
+    }
+};
