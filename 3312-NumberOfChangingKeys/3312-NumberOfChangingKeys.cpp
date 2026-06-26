1f9440b16eedcafda000499509d0d20ce14044d1
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-26 22:28:38 +0530

    Create 3312-NumberOfChangingKeys

---
+++ 3312-NumberOfChangingKeys/3312-NumberOfChangingKeys.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/26/2026, 10:28:38 PM
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int ans = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (tolower(s[i]) != tolower(s[i - 1])) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
