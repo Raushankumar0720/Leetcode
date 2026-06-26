4d0d455fb56d75b01cea109607333bf4dce70122
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-26 22:28:44 +0530

    Create 1970-SortingTheSentence

---
+++ 1970-SortingTheSentence/1970-SortingTheSentence.cpp
@@ -0,0 +1,28 @@
+// Last updated: 6/26/2026, 10:28:43 PM
+class Solution {
+public:
+    string sortSentence(string s) {
+        vector<string> arr(10);
+
+        stringstream ss(s);
+        string word;
+
+        while (ss >> word) {
+            int pos = word.back() - '0';
+
+            word.pop_back();
+            arr[pos] = word;
+        }
+
+        string ans = "";
+        for (int i = 1; i <= 9; i++) {
+            if (arr[i] != "") {
+                ans += arr[i] + " ";
+            }
+        }
+
+        ans.pop_back();
+
+        return ans;
+    }
+};
