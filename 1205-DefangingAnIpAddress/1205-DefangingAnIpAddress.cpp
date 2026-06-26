342f4da19022ecb4ff7f541f429d33d3531b5eca
Raushan kumar <raushan.singh.cg@gmail.com>
2026-06-26 22:29:06 +0530

    Create 1205-DefangingAnIpAddress

---
+++ 1205-DefangingAnIpAddress/1205-DefangingAnIpAddress.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/26/2026, 10:29:06 PM
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string result;
+        for (char c : address) {
+            if (c == '.') {
+                result += "[.]";
+            } else {
+                result += c;
+            }
+        }
+        return result;
+    }
+};
