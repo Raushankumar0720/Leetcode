c7959bfc123b18fe20001e711f11b8acc8d24a74
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-03 23:20:36 +0530

    Create 16-3sumClosest

---
+++ 16-3sumClosest/16-3sumClosest.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/3/2026, 11:20:35 PM
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int closest = nums[0] + nums[1] + nums[2];
+        for(int i=0; i<nums.size()-2; i++){
+            int l = i+1, r = nums.size()-1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(abs(sum - target) < abs(closest - target))
+                    closest = sum;
+                if(sum < target) l++;
+                else if(sum > target) r--;
+                else return sum;            // exact match krega tb 
+            }
+        }
+        return closest;
+    }
+};
