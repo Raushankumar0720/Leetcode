0b0566f31c3438132927bb2fb3ccdf71373969c6
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:27:57 +0530

    Create 2048-BuildArrayFromPermutation

---
+++ https:/github.com/Raushankumar0720/Leetcode/2048-BuildArrayFromPermutation/2048-BuildArrayFromPermutation.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/4/2026, 11:27:56 PM
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector <int> ans;
+
+        for( int i=0; i< nums.size() ; i++){
+            ans.push_back(nums[nums[i]]);
+        }
+        return ans;
+    }
+};
