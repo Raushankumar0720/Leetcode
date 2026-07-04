e9270fda817277e72a807540cbdd2f28e384aa4e
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:28:12 +0530

    Create 1482-HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ https:/github.com/Raushankumar0720/Leetcode/1482-HowManyNumbersAreSmallerThanTheCurrentNumber/1482-HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/4/2026, 11:28:11 PM
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+        
+        unordered_map<int,int> mp;
+        for(int i = 0; i < sorted.size(); i++) {
+            if(mp.find(sorted[i]) == mp.end()) {
+                mp[sorted[i]] = i; // first occurrence index = count of smaller numbers
+            }
+        }
+        
+        vector<int> result;
+        for(int num : nums) {
+            result.push_back(mp[num]);
+        }
+        return result;
+    }
+};
