8615fdf5cd7f97c0a7faba641ee8e1566422481e
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:28:42 +0530

    Create 496-NextGreaterElementI

---
+++ https:/github.com/Raushankumar0720/Leetcode/496-NextGreaterElementI/496-NextGreaterElementI.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/4/2026, 11:28:41 PM
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        for(int i = 0; i < nums1.size(); i++) {
+            int nextGreater = -1; // har element ke liye fresh
+            for(int j = 0; j < nums2.size(); j++) {
+                if(nums2[j] == nums1[i]) {
+                    for(int k = j+1; k < nums2.size(); k++) {
+                        if(nums2[k] > nums1[i]) {
+                            nextGreater = nums2[k];
+                            break;
+                        }
+                    }
+                    break; // index mil gaya
+                }
+            }
+            ans.push_back(nextGreater);
+        }
+        return ans;
+    }
+};
