46b2624de27d9c27793024c5079aa1e05fccf845
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:28:24 +0530

    Create 1019-SquaresOfASortedArray

---
+++ https:/github.com/Raushankumar0720/Leetcode/1019-SquaresOfASortedArray/1019-SquaresOfASortedArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/4/2026, 11:28:23 PM
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        int left = 0, right = n - 1;
+        int pos = n - 1;
+        
+        while(left <= right) {
+            if(abs(nums[left]) > abs(nums[right])) {
+                result[pos--] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[pos--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+};
