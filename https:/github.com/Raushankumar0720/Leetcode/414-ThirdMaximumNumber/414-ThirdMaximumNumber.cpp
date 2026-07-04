4a85677c8e44369f476f74ac31fd18aa997f0cdf
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:28:46 +0530

    Create 414-ThirdMaximumNumber

---
+++ https:/github.com/Raushankumar0720/Leetcode/414-ThirdMaximumNumber/414-ThirdMaximumNumber.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/4/2026, 11:28:46 PM
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long max3 = LONG_MIN, max2 = LONG_MIN, max1 = LONG_MIN;
+        for (int num : nums) {
+            if (num == max1 || num == max2 || num == max3)
+                continue; // agar same num aaye to skip kr do
+
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }
+
+            else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+        }
+        if (max3 == LONG_MIN)
+            return max1;
+        return max3;
+    }
+};
