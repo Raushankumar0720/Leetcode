26b598e52b58e5287449406b93be84a823e738ad
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:27:52 +0530

    Create 3371-HarshadNumber

---
+++ https:/github.com/Raushankumar0720/Leetcode/3371-HarshadNumber/3371-HarshadNumber.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/4/2026, 11:27:51 PM
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int temp = x;
+        
+        // Extract digits
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        
+        // Check Harshad condition
+        if (x % sum == 0) return sum;
+        return -1;
+    }
+};
