e9d6201a245d905557db63f693c5f0751bd52d6c
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:29:15 +0530

    Create 219-ContainsDuplicateIi

---
+++ https:/github.com/Raushankumar0720/Leetcode/219-ContainsDuplicateIi/219-ContainsDuplicateIi.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/4/2026, 11:29:14 PM
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mp; // value -> last index
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.count(nums[i]) && i - mp[nums[i]] <= k) {
+                return true;
+            }
+            mp[nums[i]] = i; // update last index
+        }
+        return false;
+    }
+};
