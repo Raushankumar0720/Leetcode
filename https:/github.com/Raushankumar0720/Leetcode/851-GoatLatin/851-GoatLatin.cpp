c01874c19bcb3ef029be354c9aab683232704c61
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:28:33 +0530

    Create 851-GoatLatin

---
+++ https:/github.com/Raushankumar0720/Leetcode/851-GoatLatin/851-GoatLatin.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/4/2026, 11:28:32 PM
+class Solution {
+public:
+    string toGoatLatin(string sentence) {
+        unordered_set<char> vowels = {
+            'a','e','i','o','u',
+            'A','E','I','O','U'  
+              };      
+
+              stringstream sent(sentence);
+
+              string word;
+              string result;
+
+              int index = 1;
+
+              while(sent >> word){
+                if(vowels.count(word[0])){
+                    word += "ma";
+                }
+                else{
+                    word = word.substr(1) + word[0] + "ma";
+                }
+
+                word += string(index , 'a');
+
+                if(!result.empty()){
+                    result += " ";
+                }
+
+                result += word;
+                index++;
+              }
+              return result;
+    }
+};
