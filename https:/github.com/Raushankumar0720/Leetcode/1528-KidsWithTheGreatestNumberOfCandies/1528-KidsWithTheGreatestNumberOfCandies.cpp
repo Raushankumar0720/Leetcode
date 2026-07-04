346276854eb3a8d2b856fb303ccea1bcd9bfcfbc
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:28:10 +0530

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ https:/github.com/Raushankumar0720/Leetcode/1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/4/2026, 11:28:10 PM
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    int maxCandies = *max_element(candies.begin(), candies.end());
+    vector<bool> result;
+    for(int c : candies) {
+        result.push_back(c + extraCandies >= maxCandies);
+    }
+    return result;
+}
+
+};
