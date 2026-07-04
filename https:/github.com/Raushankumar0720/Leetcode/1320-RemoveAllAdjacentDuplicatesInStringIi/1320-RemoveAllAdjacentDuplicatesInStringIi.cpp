8638b65c1b83612968a3520f9bc15465f51f9c6f
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-04 23:28:18 +0530

    Create 1320-RemoveAllAdjacentDuplicatesInStringIi

---
+++ https:/github.com/Raushankumar0720/Leetcode/1320-RemoveAllAdjacentDuplicatesInStringIi/1320-RemoveAllAdjacentDuplicatesInStringIi.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/4/2026, 11:28:17 PM
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char, int>> st;
+
+        for (char c : s) {
+            if (!st.empty() && st.top().first == c) {
+                st.top().second++;
+                if (st.top().second == k) {
+                    st.pop(); 
+                }
+            } else {
+                st.push({c, 1});
+            }
+        }
+
+        string result = "";
+        while (!st.empty()) {
+            auto [ch, cnt] = st.top();
+            st.pop();
+            result.append(cnt, ch);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
