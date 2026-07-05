c4494c56e5dea67777d61d98f158401d6a8b26ed
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-05 23:47:28 +0530

    Create 55-JumpGame

---
+++ https:/github.com/Raushankumar0720/Leetcode/55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/5/2026, 11:47:27 PM
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+};
