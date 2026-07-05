b8b09c15c269390010d8cc60eccca1d73919bd67
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-05 23:47:26 +0530

    Create 75-SortColors

---
+++ https:/github.com/Raushankumar0720/Leetcode/75-SortColors/75-SortColors.js
@@ -0,0 +1,10 @@
+// Last updated: 7/5/2026, 11:47:25 PM
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    return nums.sort((a,b)=>
+        a-b
+    )
+};
