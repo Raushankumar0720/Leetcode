40a86452f7f589acd7c21997b01de4de32a9c4cd
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-19 23:00:26 +0530

    Create 424-LongestRepeatingCharacterReplacement

---
+++ https:/github.com/Raushankumar0720/Leetcode/424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/19/2026, 11:00:27 PM
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+        int left = 0, maxFreq = 0, result = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            freq[s[right] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            result = max(result, right - left + 1);
+        }
+        return result;
+    }
+};
