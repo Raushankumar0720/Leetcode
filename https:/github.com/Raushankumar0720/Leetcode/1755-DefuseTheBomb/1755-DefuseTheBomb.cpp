74e47f8deb5c8012d085c551e69088fc14d8303a
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-29 00:06:55 +0530

    Create 1755-DefuseTheBomb

---
+++ https:/github.com/Raushankumar0720/Leetcode/1755-DefuseTheBomb/1755-DefuseTheBomb.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/29/2026, 12:06:54 AM
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> res(n, 0);
+
+        if (k == 0)
+            return res;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            if (k > 0) {
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n];
+                }
+
+            } else {
+                for (int j = 1; j <= -k; j++) {
+                    sum += code[(i - j + n) % n];
+                }
+            }
+            res[i] = sum;
+        }
+        return res;
+    };
+};
