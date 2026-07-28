6ef824d78fc135b563d6c611f51cfb0058151e82
Raushan kumar <raushan.singh.cg@gmail.com>
2026-07-29 00:07:08 +0530

    Create 1086-DivisorGame

---
+++ https:/github.com/Raushankumar0720/Leetcode/1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/29/2026, 12:07:07 AM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n % 2 == 0) return true;
+        else{
+            return false;
+        }
+    }
+};
