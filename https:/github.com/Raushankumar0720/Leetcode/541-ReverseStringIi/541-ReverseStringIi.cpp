72af97cb49efa53d96cf4c50a6ab8b937a0938c5
Raushan kumar <raushan.singh.cg@gmail.com>
2026-08-04 17:36:25 +0530

    Create 541-ReverseStringIi

---
+++ https:/github.com/Raushankumar0720/Leetcode/541-ReverseStringIi/541-ReverseStringIi.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/4/2026, 5:36:25 PM
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size();
+        for (int i = 0; i < n; i += 2*k) {
+           
+            int left = i;
+            int right = min(i + k - 1, n - 1);
+            while (left < right) {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
+
