cea61a08411ff698cea624c8c8d45c7cd7d386d9
Raushan kumar <raushan.singh.cg@gmail.com>
2026-08-14 22:24:16 +0530

    Create 141-LinkedListCycle

---
+++ https:/github.com/Raushankumar0720/Leetcode/141-LinkedListCycle/141-LinkedListCycle.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/14/2026, 10:24:17 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == nullptr) return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
