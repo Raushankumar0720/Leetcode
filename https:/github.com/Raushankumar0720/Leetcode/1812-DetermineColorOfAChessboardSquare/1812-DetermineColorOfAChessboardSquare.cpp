248cb48aeeb5f8296413211d86a0af244fcfcf2f
Raushan kumar <raushan.singh.cg@gmail.com>
2026-09-08 09:49:51 +0530

    Create 1812-DetermineColorOfAChessboardSquare

---
+++ https:/github.com/Raushankumar0720/Leetcode/1812-DetermineColorOfAChessboardSquare/1812-DetermineColorOfAChessboardSquare.cpp
@@ -0,0 +1,10 @@
+// Last updated: 9/8/2026, 9:49:49 AM
+class Solution {
+public:
+    bool squareIsWhite(string c) {
+        int sum = 0;
+        if(c[0] == 'a' || c[0] == 'c' || c[0] == 'e' || c[0] == 'g') sum++;
+        if(c[1] == '1' || c[1] == '3' || c[1] == '5' || c[1] == '7') sum++;
+        return sum%2==1; 
+    }
+};
