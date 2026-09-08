e48f7fa2e33fb587413f24992bf7354730d71199
Raushan kumar <raushan.singh.cg@gmail.com>
2026-09-08 09:51:40 +0530

    Create 1004-MaxConsecutiveOnesIii

---
+++ https:/github.com/Raushankumar0720/Leetcode/1004-MaxConsecutiveOnesIii/1004-MaxConsecutiveOnesIii.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/8/2026, 9:51:38 AM
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l = 0, r = 0, zeroCount = 0, maxLen = 0;
+        while (r < nums.size()) {
+            if (nums[r] == 0) zeroCount++;
+            while (zeroCount > k) {
+                if (nums[l] == 0) zeroCount--;
+                l++;
+            }
+            maxLen = max(maxLen, r - l + 1);
+            r++;
+        }
+        return maxLen;
+    }
+};
