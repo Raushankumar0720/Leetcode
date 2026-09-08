fdfdc98de5c48508a4386aa2fae1758e885f99a5
Raushan kumar <raushan.singh.cg@gmail.com>
2026-09-08 09:52:29 +0530

    Create 704-BinarySearch

---
+++ https:/github.com/Raushankumar0720/Leetcode/704-BinarySearch/704-BinarySearch.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/8/2026, 9:52:28 AM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0 , right = nums.size()-1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2 ;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+};
