cea6ad0e9ce1eee5a9efb661b0755390290185e2
Raushan kumar <raushan.singh.cg@gmail.com>
2026-09-08 09:52:18 +0530

    Create 876-MiddleOfTheLinkedList

---
+++ https:/github.com/Raushankumar0720/Leetcode/876-MiddleOfTheLinkedList/876-MiddleOfTheLinkedList.cpp
@@ -0,0 +1,26 @@
+// Last updated: 9/8/2026, 9:52:17 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == nullptr) return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
