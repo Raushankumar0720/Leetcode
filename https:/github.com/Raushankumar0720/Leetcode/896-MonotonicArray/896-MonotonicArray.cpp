1eab3db4de94bc28be7dd2df4fa4354cadb2b251
Raushan kumar <raushan.singh.cg@gmail.com>
2026-09-08 09:52:11 +0530

    Create 896-MonotonicArray

---
+++ https:/github.com/Raushankumar0720/Leetcode/896-MonotonicArray/896-MonotonicArray.cpp
@@ -0,0 +1,13 @@
+// Last updated: 9/8/2026, 9:52:10 AM
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool inc = true, dec = true;
+        for(int  i=1; i<nums.size(); i++){
+            if(nums[i] > nums[i-1]) dec = false;
+            if(nums[i] < nums[i-1]) inc = false;
+            if(!inc && !dec) return false;
+        }
+        return true;
+    }
+};
